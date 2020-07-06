7c9062a739907fab78dee7926b87af304c404df5
shinsunyoung <s2017i30@e-mirim.hs.kr>
2020-07-06 10:39:44 +0900

    커스텀 어노테이션 추가

--- spring-aop/src/main/java/me/shinsunyoung/aop/config/LogExclusion.java
+++ spring-aop/src/main/java/me/shinsunyoung/aop/config/LogExclusion.java
@@ -5,8 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.TYPE, ElementType.METHOD})
+@Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LogExclusion {
 
 }
+
