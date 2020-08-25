b6a88a8b9a55b8e737ac98ffc753d2a236be0e6d
shinsunyoung <s2017i30@e-mirim.hs.kr>
2020-08-25 17:17:17 +0900

    spring-event 비동기처리 추가

--- spring-event/src/main/java/me/shinsunyoung/springevent/SpringEventApplication.java
+++ spring-event/src/main/java/me/shinsunyoung/springevent/SpringEventApplication.java
@@ -2,8 +2,9 @@ package me.shinsunyoung.springevent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-
+@EnableAsync
 @SpringBootApplication
 public class SpringEventApplication {
 
--- spring-event/src/main/java/me/shinsunyoung/springevent/event/handler/SmsEventHandler.java
+++ spring-event/src/main/java/me/shinsunyoung/springevent/event/handler/SmsEventHandler.java
@@ -8,14 +8,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class SmsEventHandler {
 
+  @Async
   @EventListener
-  public void sendSms(RegisteredEvent event) {
+  public void sendSms(RegisteredEvent event) throws InterruptedException {
+    Thread.sleep(2000);
     System.out.println(event.getName() + "님에게 가입 축하 메세지를 전송했습니다.");
   }
 
   @Async
   @EventListener
-  public void makeCoupon(RegisteredEvent event) {
+  public void makeCoupon(RegisteredEvent event) throws InterruptedException {
+    Thread.sleep(1000);
     System.out.println(event.getName() + "님에게 쿠폰을 전송했습니다.");
   }
 }
+
+
