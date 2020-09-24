120878cd42337291cffb060ecede3ad77448d838
shinsunyoung <s2017i30@e-mirim.hs.kr>
2020-09-24 12:50:23 +0900

    Add tel to Obj

--- spring-validation/src/main/java/me/shinsunyoung/springvalidation/dto/Obj.java
+++ spring-validation/src/main/java/me/shinsunyoung/springvalidation/dto/Obj.java
@@ -5,8 +5,10 @@ import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.shinsunyoung.springvalidation.annotation.Tel;
 
-@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class Obj {
 
@@ -16,8 +18,20 @@ public class Obj {
   @NotNull(message = "나이")
   private Integer age;
 
+  @Tel
+  private String tel;
+
   @Builder
-  public Obj(@NotBlank(message = "이름") String name) {
+  public Obj(@NotBlank(message = "이름") String name,
+      @NotNull(message = "나이") Integer age) {
     this.name = name;
+    this.age = age;
+  }
+
+  public Obj(@NotBlank(message = "이름") String name,
+      @NotNull(message = "나이") Integer age, String tel) {
+    this.name = name;
+    this.age = age;
+    this.tel = tel;
   }
 }
