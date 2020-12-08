695d2b905e44ab37f56b8dc35333ca7adc8f2701
shinsunyoung <s2017i30@e-mirim.hs.kr>
2020-12-08 12:07:35 +0900

    [spring-security] 로그인 정보를 담을 DTO 추가

--- spring-security/src/main/java/me/shinsunyoung/springsecurity/dto/UserInfoDto.java
+++ spring-security/src/main/java/me/shinsunyoung/springsecurity/dto/UserInfoDto.java
@@ -1,13 +1,21 @@
 package me.shinsunyoung.springsecurity.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import me.shinsunyoung.springsecurity.domain.UserInfo;
 
+@NoArgsConstructor
 @Getter
 @Setter
 public class UserInfoDto {
   private String email;
   private String password;
-
   private String auth;
+
+  public UserInfoDto(UserInfo userInfo) {
+    this.email = userInfo.getEmail();
+    this.password = userInfo.getPassword();
+    this.auth = userInfo.getAuth();
+  }
 }
